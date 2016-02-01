2a031d22c4eb4c6befbb633ff69a29ec69ffd6d3
JC Meynard <jcmeynard@gmail.com>
2016-02-01 17:17:39 +0100

    add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+// Import section
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Hello World
+public class HelloWorld extends HttpServlet {
+
+    public void init() throws ServletException
+    {
+	// Do nothing
+    }
+
+    public void doGet(HttpServletRequest request,
+		      HttpServletReponse reponse)
+		throws ServletException, IOException
+    {
+	// response type
+	reponse.setContentType("text/html");
+
+	// Body
+	PrinterWriter out = reponse.getWriter();
+	out.println("<h1>Hello World</h1>");
+    }
+
+    public void destroy()
+    {
+	// do nothing
+    }
+
+}
